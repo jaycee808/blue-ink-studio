68b8cfb6bfddda864f25399ac0c62127b4414c9d
jaycee808 <jamie.clark@live.co.uk>
2025-05-31 16:12:19 +0100

    created sanity client to fetch information and added styles to project page

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,7 +1,55 @@
-export default function Work() {
+import { projectClient } from '../../sanity/lib/client.js'
+
+export const revalidate = 60
+
+async function getProjects() {
+    const query = `*[_type == "project"] | order(_createdAt desc){
+        _id,
+        title,
+        slug,
+        description,
+        tech,
+        url,
+        image {
+        asset->{
+            _id,
+            url
+        }
+        }
+    }`
+
+    return await projectClient.fetch(query)
+}
+
+export default async function WorkPage() {
+    const projects = await getProjects()
+
     return (
-        <section>
-            <h1>The Work Page</h1>
+        <section className="work-container">
+        <h1 className="work-title">My Work</h1>
+        <div className="project-display">
+            {projects.map((project) => (
+            <div key={project._id} className="project-image-container">
+                <img
+                src={project.image?.asset?.url}
+                alt={project.title}
+                className="project-image"
+                />
+                <h2 className="project-title">{project.title}</h2>
+                <p className="project-description">{project.description}</p>
+                <p className="project-tech">{project.tech?.join(', ')}</p>
+                {project.url && (
+                <a
+                    href={project.url}
+                    className="project-link"
+                    target="_blank"
+                >
+                    View Project →
+                </a>
+                )}
+            </div>
+            ))}
+        </div>
         </section>
     )
 }

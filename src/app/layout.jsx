2e647779084476a8f80041b361da46afd9e054db
jaycee808 <jamie.clark@live.co.uk>
2025-07-06 15:34:16 +0100

    added wrapper for bg image on Navbar and Hero section only

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,21 @@
 import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Hero from './components/Hero'
 
 export const metadata = {
   title: 'Blue Ink Studio',
-  description: 'Inked with Intent.',
+  description: 'Digital Design with Purpose.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <div className="hero-bg-wrapper">
         <Navbar />
+        <Hero />
+        </div>
         {children}
         <Footer />
       </body>

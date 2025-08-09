ddcb7c743a1724f2d6e9be117115152c41425fde
jaycee808 <jamie.clark@live.co.uk>
2025-08-09 21:00:31 +0100

    updated responsive styles

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,8 +4,8 @@ export default function Contact() {
     return (
         <section className="contact-page-section">
             <div className="contact-page-container">
-                <span className="contact-page-label">Let’s Connect</span>
-                <h1 className="contact-page-title">Start the Conversation</h1>
+                <span className="contact-page-label">Contact</span>
+                <h1 className="contact-page-title">Let's Connect</h1>
                 <ContactForm />
             </div>
         </section>
